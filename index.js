30fa9f04692a3e3d2a871da5897068e38005baf8
Nahla Shiri <nahla.job@gmail.com>
2020-06-01 12:53:09 +0100

    add and custom views

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const express = require("express");
 const path = require("path");
 
+
 /**
  * App Variables
  */
@@ -18,13 +19,43 @@ const port = process.env.PORT || "4000";
  *  App Configuration
  */
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
 /**
- * Routes Definitions
+ * Middelware Definitions
  */
 
+var requestTime = function (req, res, next) {
+    const date = new Date();
+    const day = date.getDay();
+    const hour =date.getHours();
+    if(hour > 8 && hour <18 && day > 0 && day < 6)
+       {req.requestTime = true; } 
+    else
+       {req.requestTime = false;} 
+    console.log(req.requestTime);
+    next()
+  }
+  
+  app.use("*",requestTime)
+
+ /**
+ * Routes Definitions
+ */ 
+
 app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
+    res.render("index", { title: "Home" , opening_hours : req.requestTime});
   });
+
+app.get("/services", (req, res) => {
+    res.render("services", { title: "Our services" , opening_hours : req.requestTime});
+  }); 
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { title: "Contact us" , opening_hours : req.requestTime });
+  }); 
 
 /**
  * Server Activation
