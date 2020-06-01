854ee30b43bfc33bf323c5d5c23db020f8ce7ee5
Nahla Shiri <nahla.job@gmail.com>
2020-06-01 10:12:11 +0100

    initial project

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const app = express();
-const port = 4000;
+// index.js
 
-app.listen(port, function(){
-  console.log('The server is running, ' +
-      ' please, open your browser at http://localhost:%s', 
-      port);
-});
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "4000"; 
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+  });
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
